51cc1944b2a13c3fbd6222e06f7ba8364838a3ac
Charlene Nicol <charln.nicol@gmail.com>
2022-11-15 16:42:15 +0100

    seeds and gem add

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts 'Creating posts...'
+10.times do
+  article = Article.new(
+    title: Faker::Lorem.words(number: 6),
+    content: Faker::Lorem.paragraph
+  )
+  article.save
+end
+puts 'Finished!'
